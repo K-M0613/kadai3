f7ca8592cad96dfb85199fc9e466344dd397ccdd
K-M0613 <yishumenma245@gmail.com>
2022-06-14 13:25:31 +0000

    tyuudan2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_path(user.id)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
 end
