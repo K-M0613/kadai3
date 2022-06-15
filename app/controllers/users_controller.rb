7ea4c30e2da51796bdb522d8672293152567a185
K-M0613 <yishumenma245@gmail.com>
2022-06-15 12:56:39 +0000

    tyuudan3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+    @user = current_user
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @user = current_user
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
     redirect_to users_path(user.id)
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction)
