15ca5aed7a6b0d1ad7643f63fabb942aafe26642
K-M0613 <yishumenma245@gmail.com>
2022-06-17 12:29:24 +0000

    tyuudan4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
     @user = current_user
+    @book = Book.new
   end
 
   def show
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
     if user.update(user_params)
        flash[:success] = "You have updated user successfully."
        redirect_to users_path(user.id)
-   end
+    end
   end
 
   private
