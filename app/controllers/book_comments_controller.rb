10ce99e3dc57ce29a582bfbc3b081f774b1d3a91
K-M0613 <yishumenma245@gmail.com>
2022-06-25 07:15:33 +0000

    ouyou6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,20 @@
 class BookCommentsController < ApplicationController
   before_action :correct_user, only: [:destroy]
-  
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   def destroy
-     BookComment.find(params[:id]).destroy
-     redirect_to book_path(params[:book_id])
+     @book = Book.find(params[:book_id])
+     @book_comment = BookComment.find_by(id: params[:id],book_id: params[:book_id])
+     @book_comment.destroy
+     @book_comment_new = BookComment.new
   end
 
   private
@@ -19,10 +22,10 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
   def correct_user
-    @book = Book.find(params[:id])
-    @user = @book.user
+    @book_comment = BookComment.find(params[:id])
+    @user = @book_comment.user
     redirect_to(books_path) unless @user == current_user
   end
 end
