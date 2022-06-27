c284875d04facee4ee7d5cac97710d2e681507c1
K-M0613 <yishumenma245@gmail.com>
2022-06-27 12:43:59 +0000

    ouyou7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,12 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
   end
-  
-  
+
+
     def self.looks(search, word)
       if search == "perfect_match"
          @book = Book.where("title LIKE?","#{word}")
